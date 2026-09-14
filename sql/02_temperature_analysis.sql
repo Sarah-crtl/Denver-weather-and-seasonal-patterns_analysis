eb9b5dbeacbfd70a2581b62d6b270f292404fd41
Sarah-crtl <Sarahkern0528@gmail.com>
2026-09-13 22:48:14 -0400

    Add temperature analysis SQL queries
    Added SQL queries for temperature analysis, including average temperatures by month, warmest and coldest months, and highest and lowest recorded temperatures.

--- sql/02_temperature_analysis.sql
+++ sql/02_temperature_analysis.sql
@@ -1,1 +1,47 @@
+--temperatuere analysis
 
+--Average temperature by month
+SELECT 
+  EXTRACT(MONTH FROM DATE) AS month_number,
+  FORMAT_DATE('%B', DATE) AS month,
+  ROUND(AVG(TEMP), 2) AS average_temperature
+FROM project-2-504318.global_surface_data.surface_levels
+GROUP BY
+month_number,
+month
+ORDER BY
+month_number;
+
+--Warmest and coldest months
+SELECT 
+  FORMAT_DATE('%B', DATE) AS month,
+  ROUND(AVG(TEMP), 2) AS average_temperature
+FROM project-2-504318.global_surface_data.surface_levels
+GROUP BY
+month
+ORDER BY
+average_temperature DESC;
+
+SELECT 
+  FORMAT_DATE('%B', DATE) AS month,
+  ROUND(AVG(TEMP), 2) AS average_temperature
+FROM project-2-504318.global_surface_data.surface_levels
+GROUP BY
+month
+ORDER BY
+average_temperature ASC;
+
+--Highest and lowest temperatures recorded
+SELECT
+  DATE,
+  TEMP
+FROM project-2-504318.global_surface_data.surface_levels
+ORDER BY TEMP DESC
+LIMIT 10;
+
+SELECT
+  DATE,
+  TEMP
+FROM project-2-504318.global_surface_data.surface_levels
+ORDER BY TEMP ASC
+LIMIT 10;
