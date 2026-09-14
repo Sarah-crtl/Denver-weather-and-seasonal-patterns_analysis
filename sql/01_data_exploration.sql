bce2ea8722f94c72324be0c1410c2ce2e9fa3eab
Sarah-crtl <Sarahkern0528@gmail.com>
2026-09-13 23:20:51 -0400

    Clean up SQL queries in data exploration file
    Removed comments and unnecessary whitespace in SQL queries.

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -7,13 +7,9 @@ SELECT *
 FROM project-2-504318.global_surface_data.surface_levels
 LIMIT 20;
 
--- Count the number of records
-
 SELECT
 COUNT(*) AS total_rows
 FROM project-2-504318.global_surface_data.surface_levels
-
--- Check the beginning and end dates
 
 SELECT
   MIN(DATE) AS start_date,
